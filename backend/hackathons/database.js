4def51241c4439d8f3db848c344ed2434268d588
Sai Koundinya Veluri <166689072+SAIKOUNDINYAVELURI@users.noreply.github.com>
2025-08-08 01:38:00 +0530

    Update database.js

--- backend/hackathons/database.js
+++ backend/hackathons/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '08052005@sql',
+  password: '',
   database: 'hackathons'
 });
 
@@ -41,3 +41,4 @@ module.exports = {
   insertHackathonData,
   checkHackathonExists,
 };
+
